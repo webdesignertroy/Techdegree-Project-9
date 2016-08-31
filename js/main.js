ee713d897ef8a6f1c7d405e7899e87afe2d8fe1c
webdesignertroy <sava.rowtham@yahoo.com>
2016-08-31 06:43:39 -0700

    Fixed Internet Explorer 11 issues

--- js/main.js
+++ js/main.js
@@ -526,7 +526,7 @@ $(document).ready(function(){
 						profile = members.memberData[index].profile;
 						email = members.memberData[index].email;
 						join = members.memberData[index].join;
-						newMemberList.push({name, profile, email, join});
+						newMemberList.push({name: name, profile: profile, email: email, join: join});
 					}
 				}
 
@@ -554,45 +554,41 @@ $(document).ready(function(){
 
 		searchForm: function(value) {
 
-		// Create an array of member choices
-		var sel = document.getElementById("list");
-		var searched = [];
-		var given = value.toLowerCase();
-		for ( i = 0; i < members.memberData.length; i++ ) {
-			var memberItem = members.memberData[i].first + " " + members.memberData[i].last;
-			memberItem = memberItem.toLowerCase();
-			if ( memberItem.indexOf(given) !== -1 ) {
-				if (given !== "" ) {
-					searched.push(memberItem);
+			// Create an array of member choices
+			var sel = document.getElementById("list");
+			var searched = [];
+			var given = value.toLowerCase();
+			for ( i = 0; i < members.memberData.length; i++ ) {
+				var memberItem = members.memberData[i].first + " " + members.memberData[i].last;
+				memberItem = memberItem.toLowerCase();
+				if ( memberItem.indexOf(given) !== -1 ) {
+					if (given !== "" ) {
+						searched.push(memberItem);
+					}
 				}
 			}
-		}
 
-		//  Remove previous results from #list li
-		var selExists = sel.getElementsByTagName("li")[0];
-		if ( typeof selExists !== "undefined" ) {
-			var selLength = sel.getElementsByTagName("li").length;
-			for ( i = 0; i < selLength  ; i++ ) {
-				if ( typeof sel.getElementsByTagName("li")[i] !== "undefined" ) {
-					sel.getElementsByTagName("li")[i].remove();
+			//Remove list
+			$("#list").remove();
+			/*var newList = document.createElement("ul");
+			newList.setAttribute("id", "list");*/
+
+			$("#list-aid").append('<ul id="list" class="hide-div"></ul>');
+
+			var sel = document.getElementById("list");
+
+			// Propagate #list li
+			for ( i = 0; i < searched.length; i++ ) {
+				var li = document.createElement("li");
+				li.innerHTML = searched[i];
+				sel.appendChild(li);
+			}
+
+			// Hide list if no Choices
+			if ( searched.length > 0 ) {
+					$("#list").removeClass("hide-div");
 				} else {
-					sel.getElementsByTagName("li")[0].remove(); 
-				}
-			}
-		}
-
-		// Propagate #list li
-		for ( i = 0; i < searched.length; i++ ) {
-			var li = document.createElement("li");
-			li.innerHTML = searched[i];
-			sel.appendChild(li);
-		}
-
-		// Hide list if no Choices
-		if ( searched.length > 0 ) {
-				$("#list").removeClass("hide-div");
-			} else {
-				$("#list").addClass("hide-div");
+					$("#list").addClass("hide-div");
 			}
 
 		},
